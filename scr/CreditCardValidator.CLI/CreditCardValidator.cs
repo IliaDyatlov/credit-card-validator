082de1a785069d22564c136ad0485618deb755ff
IliaDyatlov <77224623+IliaDyatlov@users.noreply.github.com>
2023-03-02 21:25:39 +0300

    Moved the class to a separate file, changed the logic

---
+++ scr/CreditCardValidator.CLI/CreditCardValidator.cs
@@ -0,0 +1,70 @@
+﻿public class CreditCardValidator
+{
+    private string _card = string.Empty;
+
+    public CreditCardValidator(string card)
+    {
+        _card = card;
+    }
+
+    public string GetPaymentSystemIdentifier()
+    {
+        char[] cardNumber = _card.ToCharArray();
+        int psi = cardNumber[0];
+        switch (psi)
+        {
+            case '2':
+                return "Payment system identifier - World";
+            case '3':
+                return "Payment system identifier - Maestro or American Express";
+            case '4':
+                return "Payment system identifier - Visa";
+            case '5':
+                return "Payment system identifier - Maestro or Master Card";
+            case '6':
+                return "Payment system identifier - Maestro or American Express";
+        }
+        return "Payment system identifier belongs to another bank";
+    }
+
+    public string GetBankIdentificationNumber()
+    {
+        string bin = _card[..6];
+        return $"Bank identification number(BIN) - {bin}";
+    }
+
+    public string GetCardIdentificationNumber()
+    {
+        string cardID = _card[6..15];
+        return $"Card ID - {cardID}";
+    }
+
+    public bool GetLuhnAlgorithm()
+    {
+        int sum = 0;
+
+        for (int i = _card.Length - 1; i > -1; i--)
+        {
+            int count = _card[i];
+            if ((i + 1) % 2 == 0)
+            {
+                count *= 2;
+            }
+
+            if (count > 9)
+            {
+                count -= 9;
+                sum += count;
+            }
+        }
+
+        if (sum % 10 == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
--- scr/CreditCardValidator.CLI/Program.cs
+++ scr/CreditCardValidator.CLI/Program.cs
@@ -1,94 +1,13 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
-    static void Main(string[] args)
-    {
-        CreditCardValidator creditCardValidator = new CreditCardValidator();
-        creditCardValidator.GetCardNumber();
-    }
-}
-
- class CreditCardValidator
-{
-    public static string card;
-
-    public CreditCardValidator()
+    public static void Main()
     {
         Console.Write("Enter card number: ");
-        card = Console.ReadLine();
-    }
-
-    public void GetCardNumber()
-    {
-        GetPaymentSystemIdentifier();
-        GetBankIdentificationNumber();
-        GetCardIdentificationNumber();
-        GetLuhnAlgorithm();
-    }
-
-    static void GetPaymentSystemIdentifier()
-    {
-        char[] cardNumber = card.ToCharArray();
-        int psi = cardNumber[0];
-        if (psi == '2')
-        {
-            Console.WriteLine($"Payment system identifier - World");
-        }
-        else if (psi == '3')
-        {
-            Console.WriteLine($"Payment system identifier - Maestro or American Express");
-        }
-        else if (psi == '4')
-        {
-            Console.WriteLine($"Payment system identifier - Visa");
-        }
-        else if (psi == '5')
-        {
-            Console.WriteLine($"Payment system identifier - Maestro or Master Card");
-        }
-        else if (psi == '6')
-        {
-            Console.WriteLine($"Payment system identifier - Maestro or China Union Pay");
-        }
-    }
-
-    static void GetBankIdentificationNumber()
-    {
-        string bin = card[..6];
-        Console.WriteLine($"Bank identification number(BIN) - {bin}");
-    }
-
-    static void GetCardIdentificationNumber()
-    {
-        string cardID = card[6..15];
-        Console.WriteLine($"Card ID - {cardID}");
-    }
-
-    static void GetLuhnAlgorithm()
-    {
-        int sum = 0;
-
-        for (int i = card.Length - 1; i > -1; i--)
-        {
-            int count = card[i];
-            if ((i + 1) % 2 == 0)
-            {
-                count *= 2;
-            }
-
-            if (count > 9)
-            {
-                count -= 9;
-                sum += count;
-            }
-        }
-
-        if (sum % 10 == 0)
-        {
-            Console.WriteLine("Card number is true");
-        }
-
-        Console.WriteLine("Card number is false");
+        string card = Console.ReadLine();
+        CreditCardValidator creditCardValidator = new(card);
+        Console.WriteLine(creditCardValidator.GetPaymentSystemIdentifier());
+        Console.WriteLine(creditCardValidator.GetBankIdentificationNumber());
+        Console.WriteLine(creditCardValidator.GetCardIdentificationNumber());
+        Console.WriteLine(creditCardValidator.GetLuhnAlgorithm());
     }
 }
